f2616dfe93e7f151c8eb7b0181391efecda9b279
Abdellah Alaoui <als.abdellah@gmail.com>
2017-04-06 15:02:23 +0100

    initial commit

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ * @flow
+ */
+
+import React, { Component } from 'react';
+import {
+  AppRegistry,
+} from 'react-native';
+import App from './src'
+export default App
+AppRegistry.registerComponent('videoEditor', () => App);
---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react' 
+import {
+	Text,
+} from 'react-native'
+
+export default class App extends Component{
+	render(){
+		return (
+			<Text style={{paddingTop: '10%'}}>Video Editor</Text>
+		)
+	}
+}
